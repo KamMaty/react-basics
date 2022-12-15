92e9162ad219411711703f713f98a2944d06b66c
Kamil Matyasik <matyasik.k@wp.pl>
2022-12-15 16:22:28 +0100

    Added filtering and message when filter doesnt match data

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,10 +6,26 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 
 const ExpenseTask = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2021");
   const selectedYearHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0)
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+
   return (
     <div>
       <Card className="expenses">
@@ -17,14 +33,7 @@ const ExpenseTask = (props) => {
           selected={filteredYear}
           onChangedYear={selectedYearHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
